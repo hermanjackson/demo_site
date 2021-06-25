b82a24131a6085651efecaf8281bd5a22697d2da
Herman Jackson <hermanjackson561@gmail.com>
2021-06-25 12:59:11 -0400

    updated landing

--- src/flux.js
+++ src/flux.js
@@ -1,6 +1,35 @@
 const getState =({getstore, getActions, setStore})=>{
     return {
-        store:{},
+        store:{
+            product:[
+            {
+                id:1,
+                title:"something",
+                image:"http://placehold.jp/150x150.png"
+            },
+            {
+                id:2,
+                title:"something",
+                image:"http://placehold.jp/150x150.png"
+            },
+            {
+                id:3,
+                title:"something",
+                image:"http://placehold.jp/150x150.png"
+            },
+            {
+                id:4,
+                title:"something",
+                image:"http://placehold.jp/150x150.png"
+            },
+            {
+                id:5,
+                title:"something",
+                image:"http://placehold.jp/150x150.png"
+            },
+               
+            ]
+        },
         actions:{}
     };
 };
--- src/home.js
+++ src/home.js
@@ -1,13 +1,41 @@
-
+import  React,{ useState ,useContext} from "react";
+import { Context } from "./appContext";
+import "./styles/home.css"
 
   export const Home = ()=>{
+    const {store, actions}= useContext(Context)
+     const [merch, setMerch]= useState("")
+
+   
    
 
     return(
 
-        <div className="container">
-         <h1>hi</h1>
+      <div className="landing-container">
+        
+     
+       {store.product.map((item,index)=>{
+         console.log("im thei tem",item)
+         return(
+          <div className="container_head"key={index}>
+          <div className="card_main">
+            <h5 className="header_words">{item.title}</h5>
+            <img className ="image"src={item.image}/>
+          
+              <div className="btn_button">
+                  <button
+                
+                    className="style_button">
+                    something else
+                  </button>
+              </div>
+          </div>
+      </div>
 
-        </div>
+
+         )
+       })}
+         
+    </div>
     )
   };
